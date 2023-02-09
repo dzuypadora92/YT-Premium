52c4413ee3fc89caaa525b0c36be46ca62040b70
Duy Padora <121789347+Duypadora92@users.noreply.github.com>
2023-02-09 18:37:23 +0700

    Create CompositeGeneratedAdaptersObserver.java

---
+++ androidx/lifecycle/CompositeGeneratedAdaptersObserver.java
@@ -0,0 +1,26 @@
+package androidx.lifecycle;
+
+import java.util.HashMap;
+
+/* compiled from: PG */
+public final class CompositeGeneratedAdaptersObserver implements bcy {
+
+    /* renamed from: a */
+    private final bcp[] f11919a;
+
+    public CompositeGeneratedAdaptersObserver(bcp[] bcpArr) {
+        bcpArr.getClass();
+        this.f11919a = bcpArr;
+    }
+
+    /* renamed from: a */
+    public final void mo3716a(bda bda, bct bct) {
+        new HashMap();
+        for (bcp a : this.f11919a) {
+            a.mo51854a();
+        }
+        for (bcp a2 : this.f11919a) {
+            a2.mo51854a();
+        }
+    }
+}
