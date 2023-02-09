d9d472d7419b9aa756646d365a958f3dab5694c0
Duy Padora <121789347+Duypadora92@users.noreply.github.com>
2023-02-09 16:58:50 +0700

    Create MenuHostHelper$$ExternalSyntheticLambda0.java

---
+++ androidx/core/view/MenuHostHelper$$ExternalSyntheticLambda0.java
@@ -0,0 +1,25 @@
+package androidx.core.view;
+
+/* compiled from: PG */
+public final /* synthetic */ class MenuHostHelper$$ExternalSyntheticLambda0 implements bcy {
+
+    /* renamed from: a */
+    public final /* synthetic */ atx f11871a;
+
+    /* renamed from: b */
+    public final /* synthetic */ atz f11872b;
+
+    public /* synthetic */ MenuHostHelper$$ExternalSyntheticLambda0(atx atx, atz atz) {
+        this.f11871a = atx;
+        this.f11872b = atz;
+    }
+
+    /* renamed from: a */
+    public final void mo3716a(bda bda, bct bct) {
+        atx atx = this.f11871a;
+        atz atz = this.f11872b;
+        if (bct == bct.ON_DESTROY) {
+            atx.mo5671d(atz);
+        }
+    }
+}
