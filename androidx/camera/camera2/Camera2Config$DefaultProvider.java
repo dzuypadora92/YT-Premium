1b41c8ef70d74eb2ebdbec45164f97ab4f55ce30
Duy Padora <121789347+Duypadora92@users.noreply.github.com>
2023-02-09 16:22:50 +0700

    Create Camera2Config$DefaultProvider.java

---
+++ androidx/camera/camera2/Camera2Config$DefaultProvider.java
@@ -0,0 +1,8 @@
+package androidx.camera.camera2;
+
+/* compiled from: PG */
+public final class Camera2Config$DefaultProvider implements aay {
+    public aaz getCameraXConfig() {
+        return C1549lm.m86954c();
+    }
+}
