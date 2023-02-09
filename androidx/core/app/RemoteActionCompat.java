3cb4af6d1485e8b9b6ccd2dc7d096bcdc9178abf
Duy Padora <121789347+Duypadora92@users.noreply.github.com>
2023-02-09 16:51:40 +0700

    Add files via upload

---
+++ androidx/core/app/RemoteActionCompat.java
@@ -0,0 +1,26 @@
+package androidx.core.app;
+
+import android.app.PendingIntent;
+import androidx.core.graphics.drawable.IconCompat;
+
+/* compiled from: PG */
+public final class RemoteActionCompat implements cxs {
+
+    /* renamed from: a */
+    public IconCompat f11854a;
+
+    /* renamed from: b */
+    public CharSequence f11855b;
+
+    /* renamed from: c */
+    public CharSequence f11856c;
+
+    /* renamed from: d */
+    public PendingIntent f11857d;
+
+    /* renamed from: e */
+    public boolean f11858e;
+
+    /* renamed from: f */
+    public boolean f11859f;
+}
